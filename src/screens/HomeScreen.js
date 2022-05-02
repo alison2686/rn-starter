608598add762b4e814506ef5905ea215c1f82a46
Alison Lee <alison2686@gmail.com>
2022-05-02 13:29:54 -0700

    add ImageScreen component and button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
 
   return <View>
     <Text style={styles.text}>Hello!!!</Text>
     <Button 
-      onPress={() => props.navigation.navigate('Components')}
+      onPress={() => navigation.navigate('Components')}
       title='Go to Components Demo' 
     />
-    <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
-      <Text>Go to List Demo</Text>
-    </TouchableOpacity>
+    <Button 
+      title='Go to List Demo'
+      onPress={() => navigation.navigate('List')}
+    />
+    <Button 
+      title='Go to Image Demo'
+      onPress={() => navigation.navigate('Image')}
+    />
     </View>
 };
 
